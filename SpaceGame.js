f8ef4c68a3089480ac226739a147378d3c35d3e3
Stephanie Amundson <stephamundson@gmail.com>
2022-05-10 10:47:26 -0500

    added basic random path generation

--- SpaceGame.js
+++ SpaceGame.js
@@ -16,7 +16,7 @@ let modelViewMatrix, projectionMatrix;
 let lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 let lightAmbient = vec4(1.0, 1.0, 1.0, 1.0);
 let lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
-let lightPosition = vec4(0.0, 0.0, 1000.0, 0.0 );
+let lightPosition = vec4(0.0, 0.0, 1000.0, 0.0);
 
 let program;
 let objects;
@@ -85,7 +85,7 @@ function init() {
 		indices: spaceshipMesh.indices,
 		transform() {
 			let eyePos = getEyePosition(modelViewMatrix);
-			let shipTransform = translate(eyePos[0], eyePos[1]-5, eyePos[2]-30);
+			let shipTransform = translate(eyePos[0], eyePos[1] - 5, eyePos[2] - 30);
 			shipTransform = mult(shipTransform, rotateZ(tiltDegrees));
 			shipTransform = mult(shipTransform, translate(0, -5, -20));
 			shipTransform = mult(shipTransform, rotateX(-theta));
@@ -99,7 +99,7 @@ function init() {
 
 	let v;
 	const planet1 = {
-		vertices: v=createSphereVertices(60.0, 45.0, 45.0), 
+		vertices: v = createSphereVertices(60.0, 45.0, 45.0),
 		vao: setUpVertexObject(v, true),
 		indices: v.indices,
 		transform() { return translate(50.0, 0.0, 200.0); },
@@ -121,14 +121,16 @@ function init() {
 
 	objects = [myShip, planet1, ring];
 
-	//Initialize texture
-    let image = new Image();
-    image.src = document.getElementById("volcanoPlanetTex").src; 
-    image.onload = function() {
-        configureTexture( image, program );
-    }
+	determineRacePath(ring);
 
-	document.onkeydown = function(ev) { keyHandler(ev); };
+	//Initialize texture
+	let image = new Image();
+	image.src = document.getElementById("volcanoPlanetTex").src;
+	image.onload = function () {
+		configureTexture(image, program);
+	}
+
+	document.onkeydown = function (ev) { keyHandler(ev); };
 
 	setTimeout(updateTimer, delay);
 
@@ -154,27 +156,54 @@ function keyHandler(event) {
 	}
 }
 
-function tilt(degrees){
+function tilt(degrees) {
 	tiltDegrees -= degrees;
 	up = vec3(-Math.sin(radians(tiltDegrees)),
-			   Math.cos(radians(tiltDegrees)), 
-			   up[2]);
-	turn(0)//update direction
+		Math.cos(radians(tiltDegrees)),
+		up[2]);
+	turn(0); //update direction
 }
 
-function turn(t){
+function turn(t) {
 	theta -= t;
-	direction = multM3V3(modelViewMatrix, vec3(0, 
-						Math.sin(radians(theta)), 
-						-Math.cos(radians(theta))));
+	direction = multM3V3(modelViewMatrix, vec3(0,
+		Math.sin(radians(theta)),
+		-Math.cos(radians(theta))));
 }
 
+// randomly generates the race path outlined by ring objects
+function determineRacePath(ringObject) {
+	let lastRing = objects[2];
+	for (let count = 0; count < 19; count++) {	// path of 20 rings
+		let previousTransformation = lastRing.transform();
+		let newX = previousTransformation[0][3] + (Math.random() * (50 - -50) + -50);
+		let newY = previousTransformation[0][3] + (Math.random() * (50 - -50) + -50);
+		let newZ = previousTransformation[2][3] - 500;
+
+		let newRing = {
+			vao: ringObject.vao,
+			indices: ringObject.indices,
+			transform() {
+				let ringTransform = mult(scalem(1.2, 1.2, 1.2), rotateY(90));
+				ringTransform = mult(translate(newX, newY, newZ), ringTransform);
+				return ringTransform;
+			},
+			material: ringObject.material,
+			textured: ringObject.textured
+		};
+
+		objects.push(newRing);
+		lastRing = newRing;
+	}
+}
+
+// updates the timer by adding one second to the counter
 function updateTimer() {
 	let dt = Date.now() - (Date.now() + delay);
 	let timer = document.getElementById("timer");
 	let seconds = timer.innerHTML.substring(3);
 	let minutes = timer.innerHTML.substring(0, 2);
-	
+
 	if (parseInt(seconds) + 1 === 60) {
 		let newMinutes = parseInt(minutes) + 1;
 		minutes = newMinutes.toString();
@@ -200,13 +229,13 @@ function updateTimer() {
 function draw() {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	projectionMatrix = perspective(30.0, gl.canvas.width/gl.canvas.height, near, far);
+	projectionMatrix = perspective(30.0, gl.canvas.width / gl.canvas.height, near, far);
 	gl.uniformMatrix4fv(uniformProjection, false, flatten(projectionMatrix));
 
 	// move eye in direction
 	eye = add(eye, vec3(direction[0], -direction[1], direction[2]));
 
-	modelViewMatrix = lookAt(eye, at , up);      
+	modelViewMatrix = lookAt(eye, at, up);
 
 	objects.forEach((obj) => {
 		gl.uniform1f(gl.getUniformLocation(program, "textured"), obj.textured);
